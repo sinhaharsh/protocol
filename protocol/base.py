d5d08db7356cb5d0dd71166423044fd7b2325281
Pradeep Reddy Raamana <raamana@gmail.com>
2023-04-06 19:44:03 -0400

    definitions I am targeting the implementation towards

--- protocol/base.py
+++ protocol/base.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+# A [imaging] Parameter is a container class for a single value, with a name
+#       with methods to check for compliance and validity
+# A [imaging] Sequence is defined as a set of parameters
+#       implemented as a dict
+# A [Sequence] Protocol is an unmutable Sequence for reference
+# An Imaging Protocol is an ordered sequence of Sequences for a single session
+#       although their order isn't used or checked in any way

c713ca143a90eb70dd941db9684670032deda805
Pradeep Reddy Raamana <raamana@gmail.com>
2023-04-06 19:45:29 -0400

    great first draft of a BaseParameter class

--- protocol/base.py
+++ protocol/base.py
@@ -24,3 +24,72 @@ class Unspecified(object):
 
         return NotImplemented
 
+
+class BaseParameter(ABC):
+    """
+    Container class to support various parameter data types
+      including numerical continuous (time), categorical (site), or an array of them
+    """
+
+
+    def __init__(self,
+                 name='parameter',
+                 value=Unspecified,
+                 dtype=float,
+                 units='ms',
+                 steps=1,
+                 range=None,
+                 required=True,
+                 severity='critical',
+                 dicom_tag=None,
+                 acronym=None):
+        """constructor"""
+
+        self.required = required
+        self.severity = severity
+
+        self.value = value
+        self.dtype = dtype
+        self.units = units
+        self.range = range
+        self.steps = steps
+
+        self.name = name
+        self.acronym = acronym
+        self.dicom_tag = dicom_tag
+
+        self.decimals = 2  # numerical tolerance in decimal places
+
+
+    def compliant(self, other):
+        """Method to check if one parameter value is compatible w.r.t another,
+            either in equality or within acceptable range, for that data type.
+
+        If any of the values to be compared are Unspecified, it returns False
+        """
+
+        if self.value is Unspecified or other.value is Unspecified:
+            warn('one of the values being compared is Unspecified!', UserWarning)
+            return False
+        else:
+            return self._check_compliance(other)
+
+
+    @abstractmethod
+    def _check_compliance(self, other):
+        """Method to check if one parameter value is compatible w.r.t another,
+            either in equality or within acceptable range, for that data type.
+        """
+
+
+    def __eq__(self, other):
+        """equality is defined as compliance here"""
+
+        return self.compliant(other)
+
+
+    def __repr__(self):
+        """repr"""
+
+        return f'{self.name}({self.value})'
+

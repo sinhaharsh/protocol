f42c19aadef0244995466398b9770006af642f6e
Pradeep Reddy Raamana <raamana@gmail.com>
2023-04-06 19:44:51 -0400

    Unspecified class to prevent use of parameter values we could not query from dicom

--- protocol/base.py
+++ protocol/base.py
@@ -8,3 +8,19 @@
 # A [Sequence] Protocol is an unmutable Sequence for reference
 # An Imaging Protocol is an ordered sequence of Sequences for a single session
 #       although their order isn't used or checked in any way
+class Unspecified(object):
+    """Class to denote an unspecified value
+
+    Reasons include:
+        - not specified in the original source e.g., DICOM image header
+        - enocded as None or similar; or presumed to be default
+
+    We need this to correctly inform the downstream users of the source,
+        to prevent them from assigning default values or imputing them another way!
+    """
+
+    def __init__(self):
+        """constructor"""
+
+        return NotImplemented
+
